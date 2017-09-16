d9b005c01f2ec064abcc873ecfd01d7680982c30
virtualstyle <rob@virtualstyle.us>
2017-09-15 21:05:23 -0400

    Initial repo create/commit

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict';
+/**
+ * This is a VirtualStyle Node module.
+ *
+ * @module @virtualstyle/node-module-boilerplate
+ * @version 0.1.0
+ * @exports nodeModuleBoilerplate
+ * @license MIT
+ * @copyright 2017 Rob Wood
+ * @link https://github.com/virtualstyle/@virtualstyle/node-module-boilerplate
+ *
+ */
+const nodeModuleBoilerplate = require('./lib/node-module-boilerplate');
+module.exports = nodeModuleBoilerplate;
---
+++ test/node-module-boilerplate-test.js
@@ -0,0 +1,18 @@
+var assert = require('assert');
+var colors = require('colors');
+
+var nodeModuleBoilerplate = new (require('../lib/node-module-boilerplate'))();
+
+describe('Testing @virtualstyle/node-module-boilerplate package'.bold.underline, function () {
+
+    beforeEach(function () {});
+
+    it('nodeModuleBoilerplate is an object'.italic, function () {
+        assert(typeof nodeModuleBoilerplate === 'object');
+    });
+
+    it('nodeModuleBoilerplate.sayHello()'.italic, function () {
+        assert(nodeModuleBoilerplate.sayHello('World') === 'Hello World');
+    });
+
+});
